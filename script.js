7d2a7abd9e661767e0aaf8fb61298847d5ca5240
Yanni <yannitrim@gmail.con>
2024-07-16 18:57:19 -0400

    Made script.js and added operation functions

---
+++ script.js
@@ -0,0 +1,18 @@
+function add(n1, n2) {return n1+n2;}
+function sub(n1, n2) {return n1-n2;}
+function mul(n1, n2) {return n1*n2;}
+function divide(n1, n2) {return n1/n2;}
+
+
+function operate(val1, val2, operator) {
+    switch (operator) {
+        case "+":
+            return add(val1, val2);
+        case "-":
+            return sub(val1, val2);
+        case "*":
+            return mul(val1, val2);
+        case "/":
+            return divide(val1, val2);
+    }
+}
